d0ec6a289069b49348f4203132e1e8db788599ba
Amaro-peter <ph_amaro@id.uff.br>
2025-04-25 00:05:15 -0300

    enunciado_2

---
+++ enunciado_2.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE FUNCTION calcular_custo_movimentacao(p_volume REAL, p_categoria INTEGER) RETURNS REAL AS $$
+DECLARE
+    v_preco REAL;
+    v_custo REAL;
+BEGIN
+    SELECT preco_para_movimentar
+      INTO v_preco
+      FROM CategoriaProduto
+     WHERE id = p_categoria;
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'CategoriaProduto com id % não encontrada', p_categoria;
+    END IF;
+
+    v_custo := p_volume * v_preco;
+    RETURN v_custo;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION enunciado2(p_navio INTEGER) 
+RETURNS TABLE(
+    carga_id INTEGER,
+    porto_id VARCHAR(3),
+    custo_unloading REAL
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT Ca.id, Ca.porto_id, calcular_custo_movimentacao(Ca.peso, Pr.categoria_id)
+    FROM Carga Ca JOIN Produto Pr ON Ca.produto_id = Pr.id
+    WHERE Ca.navio_id = p_navio;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * from enunciado2(1);
+
